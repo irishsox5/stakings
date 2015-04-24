b668fcf88956eb4ee4f94cd1d8fb75188acd485e
Cornelius Murphy <corneliusmurphy@CorneliussMBP3.attlocal.net>
2015-04-24 18:13:46 -0500

    Front End Work nearly complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
 
   def show
     @user= User.find_by(id: params[:id])
+
+    #Where the User Has Been Invested In
+    @weekly_investors=User.find_by(id: params[:id]).investment_relationships.where('created_at >= ?', 1.week.ago).count
+
+    #Where The User Has Made An Investment
+    @weekly_investments=User.find_by(id: params[:id]).investor_relationships.where('created_at >= ?', 1.week.ago).count
+
+    #Where the User Has Been Invested In
+    @monthly_investors=User.find_by(id: params[:id]).investment_relationships.where('created_at >= ?', 1.month.ago).count
+
+    #Where The User Has Made An Investment
+    @monthly_investments=User.find_by(id: params[:id]).investor_relationships.where('created_at >= ?', 1.month.ago).count
+
   end
 
   def index
