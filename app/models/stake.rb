a381b946d5f59e1ac5a8af7abcb943206dd817af
Cornelius Murphy <corneliusmurphy@CorneliussMBP3.attlocal.net>
2015-04-26 12:51:17 -0500

    More Stylings

--- app/models/stake.rb
+++ app/models/stake.rb
@@ -2,6 +2,9 @@ class Stake < ActiveRecord::Base
   include PublicActivity::Common
   #tracked owner: ->(controller, model) { controller && controller.current_user }
 
+  validates :tour_id, presence: true
+  validates :tournament_id, presence: true
+
   is_impressionable
 
   belongs_to :user
