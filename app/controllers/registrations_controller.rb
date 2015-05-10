06381d0e5ed66df0f403823b315db98820b2c0cf
Cornelius Murphy <corneliusmurphy@CorneliussMBP3.attlocal.net>
2015-05-09 19:32:18 -0500

    Comments updated in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
+
+    #Information the User will need to Add after logging in with Twitter
     def account_update_params
     params.require(:user).permit(:first_name, :last_name,:email,:address, :date_of_birth, :social_security_number)
   end
