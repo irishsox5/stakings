5ad7722fbb90084d52abf90da13df8e6b295e002
James Brink <brink.james@gmail.com>
2015-05-19 06:53:42 +0000

    Configured Devise to use twitter oauth provider

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,28 +232,35 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :twitter, Rails.application.secrets['TWITTER_API_KEY'], Rails.application.secrets['TWITTER_SECRET_KEY']
+  config.omniauth :twitter, Rails.application.secrets['TWITTER_API_KEY'], Rails.application.secrets['TWITTER_SECRET_KEY'],
+    {
+    :secure_image_url => 'true',
+    :image_size => 'original',
+    :authorize_params => {
+      :force_login => 'true',
+    }
+  }
 
-  # ==> Warden configuration
-  # If you want to use other strategies, that are not supported by Devise, or
-  # change the failure app, you can configure them inside the config.warden block.
-  #
-  # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
-  # end
+    # ==> Warden configuration
+    # If you want to use other strategies, that are not supported by Devise, or
+    # change the failure app, you can configure them inside the config.warden block.
+    #
+    # config.warden do |manager|
+    #   manager.intercept_401 = false
+    #   manager.default_strategies(scope: :user).unshift :some_external_strategy
+    # end
 
-  # ==> Mountable engine configurations
-  # When using Devise inside an engine, let's call it `MyEngine`, and this engine
-  # is mountable, there are some extra configurations to be taken into account.
-  # The following options are available, assuming the engine is mounted as:
-  #
-  #     mount MyEngine, at: '/my_engine'
-  #
-  # The router that invoked `devise_for`, in the example above, would be:
-  # config.router_name = :my_engine
-  #
-  # When using omniauth, Devise cannot automatically set Omniauth path,
-  # so you need to do it manually. For the users scope, it would be:
-  # config.omniauth_path_prefix = '/my_engine/users/auth'
+    # ==> Mountable engine configurations
+    # When using Devise inside an engine, let's call it `MyEngine`, and this engine
+    # is mountable, there are some extra configurations to be taken into account.
+    # The following options are available, assuming the engine is mounted as:
+    #
+    #     mount MyEngine, at: '/my_engine'
+    #
+    # The router that invoked `devise_for`, in the example above, would be:
+    # config.router_name = :my_engine
+    #
+    # When using omniauth, Devise cannot automatically set Omniauth path,
+    # so you need to do it manually. For the users scope, it would be:
+    # config.omniauth_path_prefix = '/my_engine/users/auth'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509212623) do
+ActiveRecord::Schema.define(version: 20150519061616) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -114,21 +114,13 @@ ActiveRecord::Schema.define(version: 20150509212623) do
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
-    #from Twitter
     t.string   "username"
     t.string   "image_url"
     t.string   "screen_name"
     t.string   "location"
-    # User Will Need To input Following
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "address"
-    t.date     "date_of_birth"
-    t.integer  "social_security_number"
-    #included from Devise
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -138,6 +130,11 @@ ActiveRecord::Schema.define(version: 20150509212623) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "address"
+    t.date     "date_of_birth"
+    t.integer  "social_security_number"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
